285c264a07bb59835afe3db5d59fab2ec510b201
M. D. Thilak Acharya <68364353+ThilakAcharya@users.noreply.github.com>
2023-04-11 22:11:01 +0530

    added Hacker rank question

---
+++ Migratory_Birds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    counts={}
+    for i in range(1,6):
+        counts[i] = arr.count(i)
+    high_freequency = max(counts.values())        
+    return (list(counts.keys())[list(counts.values()).index(high_freequency)])
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
